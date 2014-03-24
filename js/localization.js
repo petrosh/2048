753d26b7c2bfefd8415ed18cf66b8ff7891f70a8
alessandro petrozzi <petrosh@petrosh.it>
2014-03-24 19:17:40 +0100

    Localization test

---
+++ js/localization.js
@@ -0,0 +1,30 @@
+String.toLocaleString({
+    
+	"en": {
+		"%game-intro": "Join the numbers and get to the <strong>2048 tile!</strong>",
+		"%restart-button": "New Game",
+		"%keep-playing-button": "Keep going",
+		"%retry-button": "Try again",
+		"%game-won": "You win!",
+		"%game-over": "Game over!"
+	},
+	
+	"it": {
+		"%game-intro": "Unisci i numeri e ottieni la <strong>tessera 2048!</strong>",
+		"%restart-button": "Nuova Partita",
+		"%keep-playing-button": "Continua",
+		"%retry-button": "Riprova",
+		"%game-won": "Hai vinto!",
+		"%game-over": "Hai perso!"
+	},
+	
+	"fr": {
+		"%game-intro": "Combinez les chiffres et obtenez la <strong>tuile 2048!</strong>",
+		"%restart-button": "Nouveau Jeu",
+		"%keep-playing-button": "Continuer",
+		"%retry-button": "Recommencer",
+		"%game-won": "Vous gagnez!",
+		"%game-over": "Vous perdez!"
+	}
+
+});
